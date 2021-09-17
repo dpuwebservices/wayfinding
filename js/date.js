84a51452af212a2c721e86569041206b0e37d92b
Christine McClure <camcclure@gmail.com>
2021-09-17 11:13:10 -0500

    formatting

--- js/date.js
+++ js/date.js
@@ -1,29 +1,23 @@
 var Today = new Date()
-    var curInfo = "";
-    var year = Today.getYear()
-    var month = Today.getMonth()
-    var date = Today.getDate()
-    var day = Today.getDay()
-
+var curInfo = "";
+var year = Today.getYear()
+var month = Today.getMonth()
+var date = Today.getDate()
+var day = Today.getDay()
 var monthArray = new Array("Jan.", "Feb.", "Mar.", "Apr.", "May.", "Jun.", "Jul.", "Aug.", "Sept.", "Oct.", "Nov.", "Dec.");
 var dayArray = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
-
 hours = Today.getHours(),
-minutes = Today.getMinutes();
-
+    minutes = Today.getMinutes();
 if (minutes < 10) {
-minutes = "0" + minutes;
+    minutes = "0" + minutes;
 }
-
 var suffix = "am";
 if (hours >= 12) {
-suffix = "pm";
-hours = hours - 12;
+    suffix = "pm";
+    hours = hours - 12;
 }
 if (hours == 0) {
- hours = 12;
+    hours = 12;
 }
-
 curInfo = dayArray[day] + ", " + monthArray[month] + " " + date + " | " + hours + ":" + minutes + " " + suffix;
-
 document.write(curInfo);
