83c59e5a5b947e80b526d9c95a7d924843968e47
Christine McClure <camcclure@gmail.com>
2021-09-17 11:11:52 -0500

    add time to date output

--- js/date.js
+++ js/date.js
@@ -7,7 +7,23 @@ var Today = new Date()
 
 var monthArray = new Array("Jan.", "Feb.", "Mar.", "Apr.", "May.", "Jun.", "Jul.", "Aug.", "Sept.", "Oct.", "Nov.", "Dec.");
 var dayArray = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
-    
-curInfo = dayArray[day] + ", " + monthArray[month] + " " + date;
+
+hours = Today.getHours(),
+minutes = Today.getMinutes();
+
+if (minutes < 10) {
+minutes = "0" + minutes;
+}
+
+var suffix = "am";
+if (hours >= 12) {
+suffix = "pm";
+hours = hours - 12;
+}
+if (hours == 0) {
+ hours = 12;
+}
+
+curInfo = dayArray[day] + ", " + monthArray[month] + " " + date + " | " + hours + ":" + minutes + " " + suffix;
 
 document.write(curInfo);
